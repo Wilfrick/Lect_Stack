a6f3ca303f21b1d5d3e1d6f8d0757f7553574675
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2020-08-22 10:23:24 +0100

    Working

--- Lect_Stack.py
+++ Lect_Stack.py
@@ -1,1 +1,39 @@
-print("Hi there")
+
+
+def main():
+    from sys import argv, exit
+    number_of_arguments = len(argv)
+    def show_help():
+        print(
+"""Usage is: python3 Lect_Stack.py [<options>] <directory>
+Options:
+    -h      show this help""")
+
+    options = {}
+    if number_of_arguments == 1 or argv[0] == "-c": # incorrect usage
+        show_help()
+        exit(0)
+    elif number_of_arguments > 2: # we have some options set
+        if argv[1] == "-h":
+            show_help()
+            exit(0)
+    else: # standard usage
+        if len(options) >= 0:
+            pass # add more logic here to deal with other options
+        else: # default behaviour
+            from scripts.
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
