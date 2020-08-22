cd47e737bfc971296bcaadf9208a64db5cd1e827
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2020-08-22 10:31:39 +0100

    Most new files

---
+++ scripts/Amalgamate_Files.py
@@ -0,0 +1,2 @@
+def Amalgamate_Files(path_to_output_file, path_to_template_file, sorted_list_of_filenames): # writes the combined file to the output path
+    pass
---
+++ scripts/Enumerate_Files.py
@@ -0,0 +1,5 @@
+def Enumerate_Files(full_path_to_directory, file_extensions = [".tex"]):
+    list_of_files_in_path = ()
+
+
+    return list_of_files_in_path
