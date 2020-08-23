81f3858fe61deb68c6aa8989fb9663dbc21e464c
Wilfrick <27958603+Wilfrick@users.noreply.github.com>
2020-08-23 11:42:05 +0100

    Halfway through example usecase files

--- scripts/Enumerate_Files.py
+++ scripts/Enumerate_Files.py
@@ -1,5 +1,6 @@
 def Enumerate_Files(full_path_to_directory, file_extensions = [".tex"]):
-    list_of_files_in_path = ()
-
-
+    list_of_files_in_path = [path for path in full_path_to_directory.iterdir() if path.is_file()]
+    for file in list_of_files_in_path:
+        print(file.name)
     return list_of_files_in_path
+
